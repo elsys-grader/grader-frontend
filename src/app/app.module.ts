af5ca4871a97396b1262f9ed5eac8ccdc8ea02fe
Nikolay Dimitrov <nikidimi@gmail.com>
2017-11-19 21:35:01 +0200

    Added navigation and needed components on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AllSubmissionsComponent } from './all-submissions/all-submissions.component';
+import { UserSubmissionsComponent } from './user-submissions/user-submissions.component';
+
+const appRoutes: Routes = [
+    { path: 'tasks', component: TasksComponent },
+    { path: 'tasks/add', component: AddTaskComponent },
+    { path: 'tasks/:id', component: AllSubmissionsComponent },
+    { path: 'tasks/:id/:user', component: UserSubmissionsComponent },
+    { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TasksComponent,
+        AddTaskComponent,
+        AllSubmissionsComponent,
+        UserSubmissionsComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        MatInputModule,
+        MatTableModule,
+        MatCardModule,
+        MatButtonModule,
+        BrowserAnimationsModule
+    ],
+        providers: [],
+        bootstrap: [AppComponent]
 })
 export class AppModule { }
